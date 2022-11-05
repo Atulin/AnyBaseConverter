b5787ad8ab6381af13236e02a272e9af30621332
Atulin <11233299+Atulin@users.noreply.github.com>
2022-11-05 01:53:51 +0100

    Added demo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,11 +1,9 @@
-﻿// using AnyBaseConverter;
-//
-// const string input = "HEXA";
-// const int b = 34;
-//
-// var res = input.AnyToDecimal(b);
-// var back = res.DecimalToAny(b);
-//
-// Console.WriteLine($"Input: {input} with base {b}\nRes2: {res}\nBack: {back}");
+﻿using AnyBaseConverter;
 
-Console.WriteLine("Hello World");
+const string input = "HEXA";
+const int b = 34;
+
+var res = input.AnyToDecimal(b);
+var back = res.DecimalToAny(b);
+
+Console.WriteLine($"Input: {input} with base {b}\nRes2: {res}\nBack: {back}");
